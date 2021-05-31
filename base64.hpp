963b45e137c7292fc757474cad3bedd24995c5b4
sieveLau <sievelau@gmail.com>
2021-05-31 13:25:26 +0800

    init

---
+++ base64.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "c/base64.h"
+#include <memory>
+#include <string>
+
+
+namespace sieve {
+namespace base64 {
+typedef unsigned char byte;
+
+template <int N> auto b64encode(byte (&bytes)[N]) -> std::string {
+  std::shared_ptr<char[]> base64_string(new char[Base64encode_len(N)]);
+  Base64encode(base64_string.get(), reinterpret_cast<const char *>(bytes), N);
+  return std::string(base64_string.get());
+}
+
+template <std::size_t SIZE>
+auto b64encode(const std::array<byte, SIZE> &bytes) -> std::string {
+  std::shared_ptr<char[]> base64_string(new char[Base64encode_len(SIZE)]);
+  Base64encode(base64_string.get(), reinterpret_cast<const char *>(bytes),
+               SIZE);
+  return std::string(base64_string.get());
+}
+} // namespace base64
+} // namespace sieve
