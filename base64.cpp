681c981bea8c34df683bd54a28e2f5dd944a8ee2
sieveLau <sievelau@gmail.com>
2021-06-04 15:55:36 +0800

    fix size to actual decode size. add new tests.

--- base64.cpp
+++ base64.cpp
@@ -5,9 +5,11 @@ auto sieve::base64::b64decode(const std::string &encoded, size_t *size) -> byte 
     const char* cstr = encoded.c_str();
     const auto arrsize = Base64decode_len(cstr);
     char* dest = new char[arrsize];
-    Base64decode(dest,cstr);
+
+    // Base64decode_len创建了足够大的数组来容纳解码，但是真正的解码后大小要看Base64decode返回的值
+    auto actual_size = Base64decode(dest,cstr);
     if(size!=nullptr) {
-        *size=arrsize;
+        *size=static_cast<size_t>(actual_size);
     }
     return reinterpret_cast<byte*>(dest);
 }
--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,10 +1,33 @@
 #include <gtest/gtest.h>
 #include "../base64.hpp"
 #include <cstring>
-TEST(base64,contentequal)
+#include <typeinfo>
+TEST(base64, sizeEqual)
 {
-    unsigned char origin[]="string";
+    unsigned char origin[] = "string";
+    auto encoded = sieve::base64::b64encode(origin);
+    size_t size;
+    auto decoded = sieve::base64::b64decode(encoded, &size);
+    EXPECT_EQ(sizeof(origin), size);
+    delete []decoded;
+}
+
+TEST(base64, typeEqual)
+{
+    unsigned char origin[] = "string";
+    auto size = sizeof(origin);
+    auto encoded = sieve::base64::b64encode(origin);
+    auto decoded = sieve::base64::b64decode(encoded);
+    // it is array, so need to put the size into typeid
+    EXPECT_EQ(typeid(origin[size]), typeid(decoded[size]));
+    delete []decoded;
+}
+
+TEST(base64, contentEqual)
+{
+    unsigned char origin[] = "string";
     auto encoded = sieve::base64::b64encode(origin);
     auto decoded = sieve::base64::b64decode(encoded);
     EXPECT_TRUE(0==std::memcmp(origin,decoded,sizeof(origin)));
+    delete []decoded;
 }
